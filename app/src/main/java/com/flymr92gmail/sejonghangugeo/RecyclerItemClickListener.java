df62c55ee0d13336106cd6b74b4239032ee175d6
Makc Petrov <flymr92gmail.com>
2018-02-17 19:47:41 +0500

    from office

--- app/src/main/java/com/flymr92gmail/sejonghangugeo/RecyclerItemClickListener.java
+++ app/src/main/java/com/flymr92gmail/sejonghangugeo/RecyclerItemClickListener.java
@@ -39,7 +39,7 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
         View childView = view.findChildViewUnder(e.getX(), e.getY());
         if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
             mListener.onItemClick(childView, view.getChildAdapterPosition(childView));
-            return true;
+            return false;
         }
         return false;
     }
